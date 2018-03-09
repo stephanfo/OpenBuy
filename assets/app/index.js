720f52cea64cc2e698543990e7b1682ee441db6c
Stephanfo <spam@stephanfo.org>
2018-03-09 23:43:52 +0100

    Adding Jquery global in webpack for templated scripts.

--- assets/app/index.js
+++ assets/app/index.js
@@ -1,3 +1,9 @@
 require('./css/app.css');
 require('font-awesome/scss/font-awesome.scss');
 require('./js/app.js');
+
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
